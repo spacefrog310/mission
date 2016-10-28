dd7a37462a951d306097769e0659332183709ae0
spacefrog310 <Manuelverheyen310@msn.com>
2016-10-28 16:38:47 +0200

    Add files via upload

--- GtaNetwork.cs
+++ GtaNetwork.cs
@@ -8,11 +8,22 @@ namespace MissionTest
 
     public class MissionTest : Script
     {
-        public MissionTest(Client player)
+        public MissionTest()
         {
             API.onResourceStart += myResourceStart;
+            
+        }
 
-            var curModel = API.getEntityModel(player.CharacterHandle); // current player model
+        public void myResourceStart( )
+        {
+            API.consoleOutput("Starting script!");
+            
+        }
+
+        public void startMission(Client sender ) 
+        {
+            
+            var curModel = API.getEntityModel(sender.CharacterHandle); // current player model
             var oldModel = API.pedNameToModel(Convert.ToString(curModel)); // the old player model (not sure)
             PedHash model = PedHash.GarbageSMY; // garbadge man model
             var create = new create();
@@ -20,7 +31,7 @@ namespace MissionTest
             int missionVehInt = 1917016601; // 1917016601 = trash truck model
             Vector3 vehRot = new Vector3(0, 0, 0); // Vehicle rotation
 
-            Vector3 missionStartCoord = player.Position; // player position
+            Vector3 missionStartCoord = sender.Position; // player position
             Vector3 missionDesCoord = new Vector3(50, 50, 50); // destination coords 
             var missionStartIcon = 78;
             var startPoint = create.shape(missionStartCoord);
@@ -29,15 +40,10 @@ namespace MissionTest
 
             startPoint.onEntityEnterColShape += (s, ent) =>
             {
-                create.mission(player, model, missionStartCoord, missionDesCoord, missionVeh, vehRot, missionVehInt, oldModel);
+                create.mission(sender, model, missionStartCoord, missionDesCoord, missionVeh, vehRot, missionVehInt, oldModel);
             };
-        }
 
-        public void myResourceStart()
-        {
-            API.consoleOutput("Starting script!");
         }
-
 
 
    }
